4308faa5ba0c7a19d10a3098a14569f4c1d92bc8
root <root@laptop-6himmjs2.home>
2020-10-15 18:34:01 +0100

    Update project

--- main.c
+++ main.c
@@ -1,12 +1,13 @@
-#include<stdio.h> 
-#include<stdlib.h> 
+#include <stdio.h> 
+#include <stdlib.h> 
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h> 
 #include <SDL2/SDL_timer.h> 
 #define HEIGHT 1000
 #define WIDTH 1000
+#define N 20
 typedef struct Objects{
-    SDL_Rect * rects;
+    SDL_Rect ** rects;
     int number;
 } Objects;
 
@@ -14,16 +15,17 @@ void DestroySDL(SDL_Renderer * renderer, SDL_Window * window);
 void InitGrid(SDL_Renderer * renderer, int n, SDL_Color color);
 void AddRectangle(SDL_Renderer * renderer, SDL_Window * window, Objects * objs, int x, int y, int s);
 SDL_Rect MakeRectangle(int x, int y, int w, int h);
-void Update(SDL_Renderer * renderer, SDL_Window * window, Objects objs);
-void DeleteRectangle(Objects * objs, int pos);
+void Update(SDL_Renderer * renderer, SDL_Window * window, Objects * objs);
+void DeleteRectangle(Objects * objs, int x, int y);
 int main() 
 { 
     int isRunning = 1;
-    int n = 20;
     Objects objs;
-    objs.rects = (SDL_Rect*) malloc(sizeof(SDL_Rect) * n);
+    objs.rects = (SDL_Rect**) malloc(sizeof(SDL_Rect*) * N);
+    for(int i = 0; i < N; i++){
+        objs.rects[i] = (SDL_Rect*) malloc(sizeof(SDL_Rect) * N);
+    }
 
-    objs.rects[0] = MakeRectangle(1,2,HEIGHT/20, WIDTH/20);
     SDL_Window * window = NULL;
     SDL_Renderer * renderer = NULL;
     
@@ -34,23 +36,19 @@ int main()
     if(SDL_CreateWindowAndRenderer(WIDTH,HEIGHT,SDL_WINDOW_SHOWN, &window, &renderer))
         SDL_Log("Error Window");
    
+
     SDL_SetWindowTitle(window, "Le jeu de la vie");
-    
-    SDL_Color color; color.r = 255; color.g = 255; color.b = 255; color.a = 255;
-    
-    InitGrid(renderer, n, color);
-    
+   
     for(int i = 0; i < 10; i++){
-        AddRectangle(renderer,window, &objs, i,i, HEIGHT/n);
+        AddRectangle(renderer,window, &objs, i,i, HEIGHT/20);
     }
     
-    Update(renderer, window, objs);
+    DeleteRectangle(&objs, 0,0);
+    
+    Update(renderer, window, &objs);
 
-    DeleteRectangle(&objs, 0);
-
-
-    Update(renderer, window, objs);
-    SDL_RenderPresent(renderer);
+    SDL_RenderPresent(renderer); 
+    
     SDL_Event event;
     while(isRunning){
         while(SDL_PollEvent(&event) != 0) {
@@ -86,35 +84,38 @@ void InitGrid(SDL_Renderer * renderer,int n,  SDL_Color color){
         SDL_RenderDrawLine(renderer,0,i*casesHorizontal,WIDTH,i*casesHorizontal);
         SDL_SetRenderDrawColor(renderer, color.r,color.g,color.b,color.a);
     }
-
 }
 
 void AddRectangle(SDL_Renderer * renderer, SDL_Window * window, Objects * objs, int x, int y, int s){
     int w = 0; int h = 0;
     SDL_GetWindowSize(window,&w, &h);
     if(x*s > w || y*s > h){ return; }
-    SDL_Rect rectangle;
-    rectangle.h = s;
-    rectangle.w = s;
-    rectangle.x = 0 + s * x;
-    rectangle.y = 0 + y * s;
-    objs->rects[objs->number++] = rectangle;
+    SDL_Rect rect;
+    rect.x = 0 + s * x;
+    rect.y = 0 + y * s;
+    rect.w = s;
+    rect.h = s;
+    objs->rects[x][y] = rect;
+    objs->number++;
 }
 
 
-void DeleteRectangle(Objects * objs, int pos){
-    free(&objs->rects[pos]);
-
-    for(int i = pos ; i < objs->number - 1; i++){
-        objs->rects[i] = objs->rects[i+1];
-    }
+void DeleteRectangle(Objects * objs, int x, int y){
+    objs->rects[x][y] = (SDL_Rect){-1,-1,-1,-1};
+    objs->number--;
 }
 
-void Update(SDL_Renderer * renderer, SDL_Window * window, Objects objs){
-    printf("%i",objs.number);
-    for(int i = 0; i < objs.number; i++){
-        SDL_SetRenderDrawColor(renderer, 255,255,255,255);
-        SDL_RenderFillRect(renderer, &objs.rects[i]);
+void Update(SDL_Renderer * renderer, SDL_Window * window, Objects * objs){
+    SDL_RenderClear(renderer);
+    InitGrid(renderer, N, (SDL_Color){255,255,255,255});
+    for(int i = 0; i < objs->number; i++){
+        for(int j = 0; j < objs->number; j++){
+            if(&objs->rects[i][j] != NULL){
+                SDL_SetRenderDrawColor(renderer, 255,255,255,255);
+                SDL_RenderFillRect(renderer, &objs->rects[i][j]);
+            }
+
+        }
     }
 }
 
