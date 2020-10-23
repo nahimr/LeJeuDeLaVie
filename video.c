28d93a6f807114322e17735795778965de49afc4
Nahim Rouidjali <nahim.rouidjali@gmail.com>
2020-10-23 20:34:51 +0100

    Adding Copyright & GNU License

--- video.c
+++ video.c
@@ -1,5 +1,13 @@
-#include <SDL2/SDL.h>
+/*
+    Copyright (C) 2020 Nahim Rouidjali
+    This program is free software: 
+    you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+*/
 
+#include <SDL2/SDL.h>
 typedef struct INIT_VIDEO
 {
     int WIDTH;
